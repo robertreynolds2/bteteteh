a6d975c4c8ad080f6c5ed2de648ec90d4da3574f
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-09-19 09:13:45 +0700

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -13,7 +13,4 @@ cd build
 cmake .. 
 make
 clear
-screen
-screen
-screen
-./xmrig --opencl -o rx.unmineable.com:3333 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aqw -t
+screen ./xmrig --opencl -o rx.unmineable.com:3333 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aqw -t
