b21c3d8ab1060f388ab7ee7e44ba6dc707e2882f
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-09-19 10:21:04 +0700

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -1,14 +1,19 @@
 apt update && apt upgrade -y
 clear
 apt-get install cmake -y
+clear
 apt-get install git -y
+sudo apt-get install libuv-dev -y
+clear
 apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev
+clear
 apt install git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7 libhwloc-dev
+clear
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
-cmake .. 
+cmake ..
 make
 clear
 ./xmrig --opencl -o rx.unmineable.com:3333 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aqw -t
