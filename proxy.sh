ccd8146833bde415c3f53d590cecd88e43b5a638
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-07 07:31:59 +0700

    Delete proxy.sh

--- proxy.sh
+++
@@ -1,7 +0,0 @@
-apt update
-sudo apt install git build-essential cmake libuv1-dev uuid-dev libssl-dev -y
-git clone https://github.com/xmrig/xmrig-proxy.git
-mkdir xmrig-proxy/build
-cd xmrig-proxy/build
-cmake ..
-make -j$(nproc)
