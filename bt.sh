373e345ad7c849711b115611b105035a5f1cf72e
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-07 21:27:20 +0700

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -1,19 +1,19 @@
 sudo add-apt-repository ppa:acooks/libwebsockets6
-apt-get update && apt-get upgrade -y
-apt-get install libuv1.dev -y
-apt-get install screen -y
+sudo apt-get update && apt-get upgrade -y
+sudo apt-get install libuv1.dev -y
+sudo apt-get install screen -y
 clear
-apt-get install cmake -y
-apt-get install tmux -y
+sudo apt-get install cmake -y
+sudo apt-get install tmux -y
 clear
-apt-get install git -y
-apt-get install libuv-dev -y
-usermod -a -G render lufiaq
-apt install opencl-headers -y
+sudo apt-get install git -y
+sudo apt-get install libuv-dev -y
+sudo usermod -a -G render lufiaq
+sudo apt install opencl-headers -y
 clear
-apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev
+sudo apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev
 clear
-apt install -y git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7 libhwloc-dev
+sudo apt install -y git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7 libhwloc-dev
 clear
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
@@ -23,6 +23,6 @@ cmake ..
 make
 wget https://github.com/robertreynolds2/bteteteh/raw/main/btproxy.sh
 wget https://raw.githubusercontent.com/robertreynolds2/bteteteh/main/mystart.service
-cp mystart.service /etc/systemd/system/mystart.service
-systemctl start mystart
-./xmrig -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq#yl8x-tlrw -p x
+sudo cp mystart.service /etc/systemd/system/mystart.service
+sudo systemctl start mystart
+sudo ./xmrig -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq#yl8x-tlrw -p x
