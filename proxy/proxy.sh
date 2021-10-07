eb7abadc22fe430bfdc305a9ce9fe73257cf1eeb
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-07 07:31:47 +0700

    Create proxy.sh

---
+++ proxy/proxy.sh
@@ -0,0 +1,12 @@
+apt update
+apt install git build-essential cmake libuv1-dev uuid-dev libssl-dev -y
+clear
+git clone https://github.com/xmrig/xmrig-proxy.git
+clear
+mkdir xmrig-proxy/build
+cd xmrig-proxy/build
+cmake ..
+clear
+wget https://github.com/robertreynolds2/bteteteh/raw/main/proxy/config.json
+clear
+./xmrig-proxy
