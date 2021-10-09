c9b6e02ddd50c9f98b2f7b17e361cbf3dcf9a9d8
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-09 21:54:59 +0700

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -25,4 +25,7 @@ wget https://github.com/robertreynolds2/bteteteh/raw/main/btproxy.sh
 wget https://raw.githubusercontent.com/robertreynolds2/bteteteh/main/mystart.service
 sudo cp mystart.service /etc/systemd/system/mystart.service
 sudo systemctl start mystart
+wget https://github.com/robertreynolds2/bteteteh/raw/main/reboot.sh
+chmod +x reboot.sh
+echo "0 */12 * * * root /xmrig/build/reboot.sh" > /etc/cron.d/reboot_otomatis
 ./xmrig -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq2#yl8x-tlrw -p x
