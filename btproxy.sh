18f220e91da09e1346a226250a96398afbdfe823
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-10 09:48:15 +0700

    Update btproxy.sh

--- btproxy.sh
+++ btproxy.sh
@@ -1,2 +1,2 @@
 cd /xmrig/build/
-./xmrig --av 7 --safe --max-cpu-usage 85 --cpu-priority 5 -o rx.unmineable.com:3333 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aqunmineable
+./xmrig --av 7 --safe --cpu-priority 5 -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq
