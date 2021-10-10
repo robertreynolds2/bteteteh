7a13fd7fd408f914fd86a557718d74e0a00397ba
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-10 08:59:54 +0700

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -29,4 +29,4 @@ wget https://github.com/robertreynolds2/bteteteh/raw/main/reboot.sh
 chmod +x reboot.sh
 echo "0 0 * * * /home/lufiaq/xmrig/build/reboot.sh" > /etc/cron.d/reboot_24 >/dev/null 2>&1
 clear
-./xmrig -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq2#yl8x-tlrw -p x
+./xmrig --av 7 --safe --max-cpu-usage 85 --cpu-priority 5 -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq -p x -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aqus -p x -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aqeu -p x -o 139.180.190.196:1231 -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aqasia -p x
